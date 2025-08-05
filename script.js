9c0721a175dacefe181ea80ca85c04c431c53ff8
esteban95-a <dpachuri@cesde.net>
2025-08-05 16:05:36 -0500

    function declarativa con retorno por consumo mensual

---
+++ script.js
@@ -0,0 +1,12 @@
+function nombreCliente(){
+    return "Juan Perez";
+}
+function numeroCliente(){
+    return "1234567890";
+}
+function obtenerConsumo(){
+  let lecturaactual1 = 256;
+  let lecturaanterior1 = 200;
+  let consumo = lecturaactual1 - lecturaanterior1;
+  return consumo;
+}
